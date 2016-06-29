dd246cba94a08d6ef8911661d852da4f5109fccc
Ramachandran G <g.rmchndrn@gmail.com>
2016-06-29 09:22:01 +0530

    no message

--- src/app/villains/villain/villain-view.component.ts
+++ src/app/villains/villain/villain-view.component.ts
@@ -8,7 +8,7 @@ import { Router, ActivatedRoute,ROUTER_DIRECTIVES } from '@angular/router';
     directives:ROUTER_DIRECTIVES
 })
 export class VillainViewComponent implements OnInit {
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,private router:Router) {
 
     }
     private id;
@@ -17,6 +17,14 @@ export class VillainViewComponent implements OnInit {
         this.sub = this.route.params.subscribe(params => {
             this.id = +params['id'];
         });
+    }
+
+    goback(){
+        console.log(this.route.snapshot._urlSegment.parent);
+        console.log(this.router.url);
+        this.router.navigate(['../','list'],{relativeTo:this.route}).then((success)=>{console.log(success)}).catch ((reason)=>{
+            console.log(reason);
+        });        
     }
 
     ngOnDestroy() {
